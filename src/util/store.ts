0cfe5bdf8afbfceec1d64d8d36c7eb5bd2ac1c3c
Donat Andelic <34493754+donatandelic@users.noreply.github.com>
2024-07-29 15:05:45 +0200

    refactor(store): remove storage warning

--- src/util/store.ts
+++ src/util/store.ts
@@ -1,23 +1,20 @@
 import { IAsyncStore, IStore, StoreConfig } from "../types/util.js";
 
-class MemoryStore implements IStore {
+export class MemoryStore implements IStore {
   message: string =
     "[MemoryStore] An in-memory store is being used. Data will not be preserved between sessions.";
   memoryStore: { [key: string]: any } = {};
 
   getItem(key: string) {
-    //console.warn(this.message);
     return this.memoryStore[key];
   }
 
   setItem(key: string, value: any): boolean {
-    //console.warn(this.message);
     this.memoryStore[key] = value;
     return false;
   }
 
   removeItem(key: string): boolean {
-    //console.warn(this.message);
     delete this.memoryStore[key];
     return false;
   }
@@ -34,9 +31,6 @@ export class Store implements IAsyncStore {
       this.#storage = options.storage;
     } else {
       if (typeof localStorage === "undefined") {
-        console.warn(
-          "[Store] No available Storage option found, MemoryStore will be used instead. Pass a Storage client to preserve data between session."
-        );
         this.#storage = new MemoryStore();
       } else {
         this.#storage = localStorage as unknown as IStore;
